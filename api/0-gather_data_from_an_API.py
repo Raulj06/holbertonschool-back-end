d44a3e44325ef6c82f6d108a6853f1e44b718b0e
Raulj06 <5015@holbertonschool.com>
2022-12-06 00:05:01 +0000

    fix typo

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,40 @@
 #!/usr/bin/python3
-"""return information employee TODO list progress from an REST API"""
-from requests import get
-from sys import argv
+"""api"""
+import requests
+import sys
 
 
-def get_api():
-    """Gather data from an API"""
-    emp_id = int(argv[1])
-    emp_name = ''
-    tasks_done = 0
-    tasks_total = 0
-    tasks_titles = []
+if __name__ == "__main__":
+    if len(sys.argv) == 2:
+        id = int(sys.argv[1])
 
-    users_res = get('https://jsonplaceholder.typicode.com/users').json()
-    for user in users_res:
-        if user['id'] == emp_id:
-            emp_name = user['name']
-            break
+    url = "https://jsonplaceholder.typicode.com/"
+    employee_name = ""
+    try:
+        response_users = requests.request("GET", "{}users/{}".format(url, id))
+        response_tasks = requests.request("GET", "{}todos/".format(url))
+        employee_name = response_users.json()["name"]
 
-    tasks_res = get('https://jsonplaceholder.typicode.com/todos').json()
-    for task in tasks_res:
-        if task['userId'] == emp_id:
-            if task['completed']:
-                tasks_titles.append(task['title'])
-                tasks_done += 1
-            tasks_total += 1
+    except KeyError:
+        pass
 
-    print('Employee {} is done with tasks({}/{}):'.format(emp_name,
-                                                          tasks_done,
-                                                          tasks_total))
-    for title in tasks_titles:
-        print('\t {}'.format(title))
+    total_number_of_tasks = len(response_tasks.json())
+    total_number_of_undone_tasks = 0
+    total_number_of_done_task = 0
 
+    for i in range(total_number_of_tasks):
+        if response_tasks.json()[i]['userId'] == id:
+            if response_tasks.json()[i]['completed']:
+                total_number_of_done_task += 1
+            elif response_tasks.json()[i]['completed'] is False:
+                total_number_of_undone_tasks += 1
 
-if __name__ == '__main__':
-    get_api()
+    total_number_of_done_or_undone_tasks = total_number_of_undone_tasks + \
+        total_number_of_done_task
+    print("Employee {} is done with tasks({}/{}):".format(employee_name,
+          total_number_of_done_task, total_number_of_done_or_undone_tasks))
+    for i in range(total_number_of_tasks):
+        if response_tasks.json()[i]['userId'] == id:
+            if response_tasks.json()[i]['completed']:
+
+                print("\t" + " " + response_tasks.json()[i]['title'])
